7c69213c2ebfc0c588fcc3b4cf1023b868f42e2f
marcelonader <marcelo.m.nader@gmail.com>
2021-06-09 09:29:08 -0300

    adiciona os desafios em subpastas

---
+++ Credit-Card-Mask/Credit-Card-Mask.js
@@ -0,0 +1,23 @@
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// maskify("4556364607935616") == "############5616"
+// maskify(     "64607935616") ==      "#######5616"
+// maskify(               "1") ==                "1"
+// maskify(                "") ==                 ""
+// maskify("Skippy")                                   == "##ippy"
+// maskify("Nananananananananananananananana Batman!") == "####################################man!"
+
+function maskify(cc) {
+    let textSecret = [];
+    for(let i = 0; i < cc.length-4; i++){
+      textSecret.push('#')
+    }
+    for(let i = cc.length-4; i < cc.length; i++){
+      textSecret.push(cc[i])
+    }
+    return textSecret.join('')
+  }
+  
---
+++ Sum-of-Digits-Digital-Root/Sum-of-Digits-Digital-Root.js
@@ -0,0 +1,30 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+    let arrayN = n.toString().split('')
+    let arrayNumber = []
+    
+    while(arrayN.length > 1) {
+        
+        for(element of arrayN){
+            arrayNumber.push(Number(element))
+        } 
+        resultado = arrayNumber.reduce((previous, current) => previous + current)
+        arrayN = resultado.toString().split('')
+        arrayNumber = []
+    
+    }
+    return Number(arrayN)
+    
+}
+
+ console.log(digital_root(493193))
+
